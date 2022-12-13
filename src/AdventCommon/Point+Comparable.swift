af3260b7d1b7b079e24e2734f7770ba6cb5b831e
Randy Eckenrode <randy@largeandhighquality.com>
2022-12-12 20:15:01 -0500

    Add Comparable implementation for Point

---
+++ src/AdventCommon/Point+Comparable.swift
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+extension Point: Comparable {
+    public static func < (lhs: Point, rhs: Point) -> Bool {
+        return lhs.x < rhs.x || (lhs.x == rhs.x && lhs.y < rhs.y)
+    }
+}
