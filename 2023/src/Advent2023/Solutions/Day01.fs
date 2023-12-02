3e084e40edde724174a87b8744898a9b9cbe56b6
Randy Eckenrode <randy@largeandhighquality.com>
2023-12-02 00:49:23 -0500

    2023: day 1

---
+++ 2023/src/Advent2023/Solutions/Day01.fs
@@ -0,0 +1,79 @@
+module Advent2023.Solutions.Day1
+
+open System.CommandLine
+open System.IO
+
+open FSharp.Control
+open FSharpx.Text
+
+open Advent2023.Support
+
+
+type Options = { Input: FileInfo }
+
+let private revString (s: string) = s |> Seq.rev |> System.String.Concat
+
+let private digits = "one|two|three|four|five|six|seven|eight|nine"
+let private reverseDigits = revString digits
+
+let private remap s =
+    match s with
+    | "one"
+    | "eno" -> "1"
+    | "two"
+    | "owt" -> "2"
+    | "three"
+    | "eerht" -> "3"
+    | "four"
+    | "ruof" -> "4"
+    | "five"
+    | "evif" -> "5"
+    | "six"
+    | "xis" -> "6"
+    | "seven"
+    | "neves" -> "7"
+    | "eight"
+    | "thgie" -> "8"
+    | "nine"
+    | "enin" -> "9"
+    | s -> s
+
+let calibrate lines =
+    [ for line in lines do
+          let first = Regex.tryMatch $"({digits}|\d)" line |> Option.map _.GroupValues
+
+          let second =
+              Regex.tryMatch $"({reverseDigits}|\d)" (revString line)
+              |> Option.map _.GroupValues
+
+          let digits =
+              match first, second with
+              | Some [ fst ], Some [ snd ] -> $"{remap fst}{remap snd}"
+              | Some [ fst ], None -> $"{remap fst}{remap fst}"
+              | x -> failwith $"line did not contain enough digits %A{x}{line}"
+
+          let value =
+              let didParse, value = System.Int32.TryParse digits
+
+              if not didParse then
+                  failwith $"{digits} is not an integer"
+              else
+                  value
+
+          yield value ]
+
+let run (options: Options) (console: IConsole) =
+    task {
+        use file = options.Input.OpenRead ()
+        use reader = new StreamReader (file)
+        let lines = TaskSeq.toList (lines reader)
+
+        let calibrationValues = calibrate lines
+        let calibrationSum = calibrationValues |> List.sum
+
+        printfn $"the sum of all of the calibration values: {calibrationSum}"
+
+        return 0
+    }
+
+let command = Command.create "day1" "Trebuchet?!" run
---
+++ 2023/tests/Advent2023.Tests/Solutions/Day01.fs
@@ -0,0 +1,42 @@
+module Advent2023.Tests.Day1
+
+open Expecto
+
+open Advent2023.Solutions.Day1
+
+[<Tests>]
+let tests =
+    testList
+        "Day 1"
+        [ testList
+              "Examples"
+              [ test "Example 1" {
+                    let expectedValues = [ 12; 38; 15; 77 ]
+                    let example = [ "1abc2"; "pqr3stu8vwx"; "a1b2c3d4e5f"; "treb7uchet" ]
+                    let values = calibrate example
+                    Expect.equal values expectedValues "the lines add up"
+                }
+
+                test "Example 2" {
+                    let expectedValues = [ 29; 83; 13; 24; 42; 14; 76 ]
+
+                    let example =
+                        [ "two1nine"
+                          "eightwothree"
+                          "abcone2threexyz"
+                          "xtwone3four"
+                          "4nineeightseven2"
+                          "zoneight234"
+                          "7pqrstsixteen" ]
+
+                    let values = calibrate example
+                    Expect.equal values expectedValues "the lines add up"
+                } ]
+
+          testList
+              "When there are multiple digits"
+              [ test "It parses only the first and last" {
+                    let expectedValues = [ 42 ]
+                    let values = calibrate [ "4294342982" ]
+                    Expect.equal values expectedValues "just 42"
+                } ] ]
