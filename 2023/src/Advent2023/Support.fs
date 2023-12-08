c49b62d56c5e58dd1c2a1815369580aa489c4fd8
Randy Eckenrode <randy@largeandhighquality.com>
2023-12-08 16:53:11 -0500

    2023: add gcd and lcm functions

--- 2023/src/Advent2023/Support.fs
+++ 2023/src/Advent2023/Support.fs
@@ -6,6 +6,7 @@ open System
 open System.CommandLine
 open System.CommandLine.NamingConventionBinder
 open System.IO
+open System.Numerics
 open System.Reflection
 open System.Text
 open System.Threading.Tasks
@@ -111,3 +112,34 @@ module ParserResult =
         function
         | Success (result, _, _) -> Result.Ok result
         | Failure (message, _, _) -> Result.Error message
+
+let inline gcd (lhs: 'a) (rhs: 'a) : 'a :> IBinaryInteger<'a> =
+    let zero = 'a.Zero
+    let one = 'a.One
+    let two = 'a.One + 'a.One
+
+    let rec loop lhs rhs counter =
+        if lhs = one && rhs = one then
+            one
+        elif lhs = rhs then
+            lhs * counter
+        else
+            let struct (lhsQuo, lhsRem) = 'a.DivRem (lhs, two)
+            let struct (rhsQuo, rhsRem) = 'a.DivRem (rhs, two)
+
+            match lhsRem = zero, rhsRem = zero with
+            | true, true -> loop lhsQuo rhsQuo counter * two
+            | true, false -> loop lhsQuo rhs counter
+            | false, true -> loop lhs rhsQuo counter
+            | false, false when lhs < rhs -> loop rhs lhs counter
+            | false, false -> loop (lhs - rhs) rhs counter
+
+    loop lhs rhs 'a.One
+
+let inline lcm lhs rhs =
+    let gcd = gcd lhs rhs
+
+    if gcd = rhs || gcd = rhs then
+        max lhs rhs
+    else
+        lhs * rhs / gcd
